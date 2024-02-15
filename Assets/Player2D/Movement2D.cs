8eb6d7ebb82e89bb2671a88c983fc36fc8fac79a
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-02-15 16:10:53 -0400

    Final

--- Assets/Player2D/Movement2D.cs
+++ Assets/Player2D/Movement2D.cs
@@ -1,24 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
-    private Rigidbody2D rb;
+    [SerializeField] public float speed = 5.0f;
+    public Rigidbody2D rb;
+    private Animator animator;
+    Vector2 MoveXY;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        MoveXY.x = Input.GetAxisRaw("Horizontal");
+        MoveXY.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb.velocity = movement * speed / Time.fixedDeltaTime;
-        Debug.Log(Time.fixedDeltaTime);
+        MovementPlayer();
+        AnimatorCharacter();
+       //float moveHorizontal = Input.GetAxis("Horizontal");
+       //float moveVertical = Input.GetAxis("Vertical");
+       //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+       //rb.velocity = movement * speed / Time.fixedDeltaTime;
+       //Debug.Log(Time.fixedDeltaTime);
+    }
+
+    void MovementPlayer()
+    {
+        rb.MovePosition(rb.position +  MoveXY.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    void AnimatorCharacter()
+    {
+        if(MoveXY != Vector2.zero)
+        {
+            animator.SetFloat("XInput", MoveXY.x);
+            animator.SetFloat("YInput", MoveXY.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+       //else
+       //{
+       //    animator.SetBool("Moving", false);
+       //}
     }
 
 }
